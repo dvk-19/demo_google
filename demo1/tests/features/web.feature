88bc825d689f3f6f8ee2f58fb32acb7fd9a081f8
Stephy Varughese (TCS) <stephy.varughese@nio.io>
2019-07-15 16:56:54 +0530

    Pushing changes in google search tests

--- demo1/tests/features/web.feature
+++ demo1/tests/features/web.feature
@@ -1,8 +1,8 @@
 @web @google
 
 Feature: Google Search Engine
- As I browse the web
- I can find required information
+ As user browse the web
+ user can find required information
 
 Scenario: Test Something
 
@@ -10,9 +10,3 @@ Scenario: Basic Google Search
  Given The Google Search Page is displayed
  When The user searches for Firefox
  Then the results are shown for Firefox
-
-Scenario: Select the link to download
- Given The results are displayed
- When We click the first link
- Then The firefox page is displayed
-
--- demo1/tests/step_defs/test_web.py
+++ demo1/tests/step_defs/test_web.py
@@ -1,39 +1,42 @@
-import selenium, pytest, os, pytest_bdd , requests , parser
-from pytest_bdd import given, parsers, scenarios, when, then, scenario
-
-import pytest_bdd
+import _pytest.fixtures
+from pytest_bdd import given, parsers, when, then, scenario
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 google_page = 'https://google.co.in'
-#phrase_1 = 'Firefox Download'
+file_path = '../features/web.feature'
+# better not to hardcode the path, instead can be fetched using os.path
 
 
-@scenario('../features/web.feature','Basic Google Search')
-
-@pytest.fixture
+@_pytest.fixtures.fixture()
 def browser():
     print('Initiating Chrome browser')
-    b = webdriver.Chrome(ChromeDriverManager().install())
-    #b = webdriver.chrom
-    #b.implicitlywait(10)
-    b.quit()
+    browser_obj = webdriver.Chrome(ChromeDriverManager().install())
+    return browser_obj
 
 
-@given('Google homepage is displayed')
-def gogoogle(browser, google_page):
+@scenario(file_path, 'Basic Google Search')
+def test_google():
+    """Basic google search"""
+
+
+@given('The Google Search Page is displayed')
+def gogoogle(browser):
     browser.get(google_page)
 
-@when(parsers.parse('The user searches for "{phrase}"'))
+
+@when(parsers.parse('The user searches for {phrase}'))
 def search_phrase(browser, phrase):
     browser.find_element_by_name('q').send_keys(phrase)
     browser.find_element_by_name('q').send_keys(Keys.RETURN)
 
-@then(parsers.parse('the results are shown for "{phrase}"'))
-def results_have_one(browser, phrase):
-    xpath = "//div[@id='links']//*[contains(text(), '%s')]" % phrase
+
+@then(parsers.parse('the results are shown for {phrase}'))
+def results_have_one(browser):
+    xpath = '//a[starts-with(@href, "https://www.mozilla.org/en-US/firefox/new/")]'
     results = browser.find_elements_by_xpath(xpath)
     assert len(results) > 0
 
 
+# Need to add a tear down fuction
